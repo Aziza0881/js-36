e30fe62a8d2eb74de7dcac83b338dc2a2543a2ca
aziza <azizabonusaliyeva@gmail.com>
2023-12-16 16:28:41 +0500

    js-36
    js-36

--- index.js
+++ index.js
@@ -9,3 +9,44 @@ let img3 = document.querySelector('.bb')
 
 
 
+
+
+
+let openBtn = document.querySelector('.open')
+let closeBtn = document.querySelector('.close')
+let modal_w = document.querySelector('.bg-modal')
+
+openBtn.onclick = () => {
+    modal_w.style.display = "flex"
+}
+
+closeBtn.onclick = () => {
+    modal_w.style.display = "none"
+}
+
+let tex = document.querySelector(".texp")
+let tex2 = document.querySelector(".texp2")
+
+let vniz = document.querySelector(".accordion1")
+let verx = document.querySelector(".accordion")
+
+
+vniz.onclick = () => {
+    if(tex.style.display === "none") {
+        tex.style.display = "flex"
+    } else {
+        tex.style.display = "none"
+    }
+}
+
+
+verx.onclick = () => {
+    if(tex2.style.display === "none") {
+        tex2.style.display = "flex"
+    } else {
+        tex2.style.display = "none"
+    }
+}
+
+
+
